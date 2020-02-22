1440bc6f77b6e91b02d2d71778a70eb43f2be6be
R1Ng0 <t1m.sadist@gmail.com>
2020-02-22 16:24:19 +0500

    changed DEBUG

--- fourAfour/settings.py
+++ fourAfour/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g2s#yg)h7yzc_73jz8entdtlhjkpim2#-05=v-li=#1ty5a*t^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
